53af12c096fefc4ac1ea55be93fe38c275e4e496
Tainá Andrade <taina.09.08@hotmail.com>
2022-04-08 22:02:58 -0300

    Update países menos e mais populosos.py

--- países menos e mais populosos.py
+++ países menos e mais populosos.py
@@ -13,49 +13,23 @@ plt.rc('figure', figsize = (20, 10))
 import warnings
 warnings.filterwarnings("ignore")
 
-
-# In[3]:
-
-
 dados = pd.read_csv('dados/world_population.csv')
-
-
-# In[4]:
-
 
 dados
 
 
-# ## Renomeando nome da coluna
-
-# In[5]:
-
-
 dados.rename(columns={'Population (2020)':'Population'}, inplace=True)
-
-
-# ## Listando países mais populosos (> cem milhões)
-
-# In[6]:
 
 
 size_cem_milhoes = dados['Population'] > 100000000
 paises_populosos = dados[size_cem_milhoes].sort_values(by='Population', ascending=False)
 
 
-# In[7]:
-
 
 paises_populosos.index = range(paises_populosos.shape[0])
 
 
-# In[8]:
-
-
 paises_populosos
-
-
-# In[81]:
 
 
 fig = plt.figure()
@@ -67,17 +41,12 @@ ax.set_ylabel('Tamanho da População', fontsize=20)
 ax.set_xlabel('Nome dos Países', fontsize=20)
 country = paises_populosos['Country/Other'].head(10)
 population = paises_populosos['Population'].head(10)
-
-
 plt.gca().set_yticklabels(['{:}'.format(x) for x in population])
-
-
 plt.yticks(population)
 ax.bar(country, population)
-
-
 plt.show()
 
+<img src="https://user-images.githubusercontent.com/27781259/162550365-f7080772-eec5-4c6f-9889-ead75f9058c6.png">
 
 # ## Listando países menos populosos (< cem milhões)
 
